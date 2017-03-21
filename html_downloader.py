c5b650c3d7e527e4e9ab0f68c83e63a3c982392e
renhongqiang <renhongqiang@bilibili.com>
2017-03-21 12:03:15 +0800

    initial the craw project

---
+++ html_downloader.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import urllib2
+
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        if url is None:
+            return None
+        response = urllib2.urlopen(url)
+
+        if response.getcode() != 200:
+            return None
+        return response.read()
---
+++ html_outputer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        fout = open('output.html', 'w')
+        fout.write('<html>')
+        fout.write('<body>')
+        fout.write('<table>')
+
+        for data in self.datas:
+            print data
+            fout.write('<tr>')
+            fout.write('<td>%s</td>' % data['url'])
+            fout.write('<td>%s</td>' % data['title'])
+            fout.write('<td>%s</td>' % data['summary'])
+            fout.write('</tr>')
+
+        fout.write('</table>')
+        fout.write('</body>')
+        fout.write('</html>')
+
+        fout.close()
